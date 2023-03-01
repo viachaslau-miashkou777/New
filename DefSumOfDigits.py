952e2046e799dd3e0d9597d3708830af24d37f58
Cap Malu <124048235+viachaslau-miashkou777@users.noreply.github.com>
2023-03-01 09:55:42 +0200

    Add files via upload

---
+++ DefSumOfDigits.py
@@ -0,0 +1,14 @@
+import csv
+
+def sum_of_digits(number):
+    if type(number) == int and 0 <= len(str(number)) <=10:
+        return sum(int(d) for d in str(number))
+    else:
+        return "Input error!"
+
+with open('test_numbers.csv', 'r') as f:
+    reader = csv.reader(f)
+    for string in reader:
+        number = int(string[0])
+        sum_digits = sum_of_digits(number)
+        print(f"Input number: {number}, Sum of digits: {sum_digits}")
